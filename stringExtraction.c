a6e8dfc410b1f58277a9e7cf273c904a218546db
cargamoni <ahmetcanirdem@hotmail.com>
2017-05-25 08:07:20 +0300

    updates

--- stringExtraction.c
+++ stringExtraction.c
@@ -1,1 +1,28 @@
 #include <stdio.h>
+
+int fileLengthShow()
+{
+  FILE *filePt;                      //Dosyası işaret eden file tipli pointer
+  filePt = fopen("todoList.txt","r");
+  int fileLength = 0;
+
+  do
+  {
+    getc(filePt);
+    fileLength++;
+  } while(getc(filePt) != EOF);
+  fclose(filePt);
+  return fileLength*2;
+}
+
+int getComments()
+{
+  FILE *filePt;
+  filePt = fopen("stringExtraction.c", "r");
+  
+}
+
+int main()
+{
+  printf("%d", fileLengthShow());
+}
