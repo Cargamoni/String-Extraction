92e756a7e6cec1eb92d6ffc70ff5e560bf5534bc
cargamoni <ahmetcanirdem@hotmail.com>
2017-05-27 04:35:48 +0300

    Clone nad getComment

--- stringExtraction.c
+++ stringExtraction.c
@@ -2,27 +2,61 @@
 
 int fileLengthShow()
 {
-  FILE *filePt;                      //Dosyası işaret eden file tipli pointer
-  filePt = fopen("todoList.txt","r");
+  FILE *filePointer;                      //Dosyası işaret eden file tipli pointer
+  filePointer = fopen("todoList.txt","r");
   int fileLength = 0;
 
   do
   {
-    getc(filePt);
+    getc(filePointer);
     fileLength++;
-  } while(getc(filePt) != EOF);
-  fclose(filePt);
+  } while(getc(filePointer) != EOF);
+  fclose(filePointer);
   return fileLength*2;
 }
 
 int getComments()
 {
-  FILE *filePt;
-  filePt = fopen("stringExtraction.c", "r");
-  
+  FILE *filePointer, *getComment;
+  filePointer = fopen("calismalar.c", "r");
+  do
+  {
+    char holder[300];
+    fgets(holder,3,filePointer);
+    if(holder[0] == '/' && holder[1] == '/')
+    {
+      int a = ftell(filePointer);
+      fseek(filePointer, -2, SEEK_CUR);
+      fgets(holder,250,filePointer);
+      printf("%s",holder);
+      printf("%d",a);
+    }
+  }while(!feof(filePointer));
+}
+
+int cloneFile(char *dir, char *cloneName)
+{
+  FILE *veriAktarma, *veriOkuma;
+  veriAktarma = fopen(cloneName, "w");
+  veriOkuma = fopen(dir, "r");
+  char a;
+  do
+  {
+    a = getc(veriOkuma);
+    fputc(a, veriAktarma);
+  }while(!feof(veriOkuma));
+  fclose(veriAktarma);
+  fclose(veriOkuma);
 }
 
 int main()
 {
+  char fileDir[50], fileName[50];
+  printf("\nKopyalanacak dosyanın tam adını giriniz: ");
+  scanf("%s", fileDir);
+  printf("\nKlon dosyanın adını giriniz : ");
+  scanf("%s", fileName);
+  cloneFile(fileDir,fileName);
   printf("%d", fileLengthShow());
+  //getComments();
 }
